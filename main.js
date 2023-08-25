86a31ed2fa42b6b1970e9571e3198f8b00e525d8
Tata123Expert <117285408+Tata123Expert@users.noreply.github.com>
2023-08-25 16:11:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function clearCanvas()
+{
+    background("white");
+}
+function draw()
+{
+    strokeWeight(12);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    var result = results[0].label;
+    document.getElementById('label').innerHTML = 'Nome: ' + result.replace('_', ' ');
+    document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new speechSynthesisUtterance(result.replace('_', ' '));
+    synth.speak(utterThis);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
